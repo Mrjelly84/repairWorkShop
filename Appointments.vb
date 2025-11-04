774f2864d30dc856d5792fc5edb3d2a478ff3fa0
Mrjelly84 <160085192+Mrjelly84@users.noreply.github.com>
2025-11-04 15:25:23 +0000

    added appointment class to project

---
+++ Appointments.vb
@@ -0,0 +1,17 @@
+﻿Public Class Appointments
+    Private adapter As New RepairServicesDataSetTableAdapters.AppointmentListTableAdapter
+    Public Shared LastError As String
+    Dim origAppointment As RepairServicesDataSet.AppointmentListRow
+
+    Public ReadOnly Property Items As DataTable
+        Get
+            Return adapter.GetData()
+        End Get
+    End Property
+
+    Public Function GetByCustomerId(custId As Short) As DataTable
+        Dim table As DataTable = adapter.GetData()
+        table.DefaultView.RowFilter = "CustId = " & custId
+        Return table
+    End Function
+End Class
