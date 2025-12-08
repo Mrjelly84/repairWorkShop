61e57cf0c4f882f540b9465687111215ee5b7ec3
Mrjelly84 <160085192+Mrjelly84@users.noreply.github.com>
2025-12-08 13:34:59 +0000

    btnCancel done & txtBoxes TODO:save function

--- frmModify.vb
+++ frmModify.vb
@@ -1,6 +1,20 @@
 ﻿Public Class frmModify
     Public Property CustomerId As Short
+    Dim cust As New Customers
     Private Sub frmModify_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim row As RepairServicesDataSet.CustomersRow = cust.FindById(CustomerId)
+        lblCustomerId.text = row.CustId.ToString()
+        txtName.Text = row.Name
+        txtPhone.Text = row.Phone
+
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        DialogResult = DialogResult.Cancel
+        Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
 
     End Sub
 End Class
